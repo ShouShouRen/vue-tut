2f681896a8648459fee341c02cd00a98006d8e70
Peter Chen <peterchen2017125@gmail.com>
2022-08-19 23:56:05 +0800

    20220819

--- vue3-tut/src/App.vue
+++ vue3-tut/src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <componentA></componentA>
+  <!-- <componentA></componentA> -->
+  <!-- <p>{{dataList[0]}}</p>
+  <p>{{dataList[1]}}</p>
+  <p>{{dataList[2]}}</p>
+  <p>{{dataList[3]}}</p> -->
+  <p v-for="text in dataList">{{text}}</p>
 </template>
 
 <script setup>
-    import componentA from './components/componentA.vue';
+    // import componentA from './components/componentA.vue';
+    const dataList = [
+      'a','b','c','d'
+    ]
 </script>
 
 
