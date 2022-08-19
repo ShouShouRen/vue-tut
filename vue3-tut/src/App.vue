d330d82f38549e172eca8292e89c3b99c301f932
Peter Chen <peterchen2017125@gmail.com>
2022-08-20 00:11:51 +0800

    20220819

--- vue3-tut/src/App.vue
+++ vue3-tut/src/App.vue
@@ -5,25 +5,54 @@
   <p>{{dataList[2]}}</p>
   <p>{{dataList[3]}}</p> -->
   <!-- <p v-for="text in dataList">{{text}}</p> -->
-  <p v-for="(value,key,index) in dataList">
+  <!-- <p v-for="(value,key,index) in dataList">
     <span v-show="key !== 'key3'">{{index}}{{key}}:{{value}}</span>
-  </p>
+  </p> -->
   <!-- <p v-for="text in dataLists">{{text}}</p> -->
+
+  <p class="test-cls" :key="test">{{ test }}</p>
 </template>
 
 <script setup>
-    // import componentA from './components/componentA.vue';
-    const dataList = [
-      'a','b','c','d','123','abcd'
-    ]
-    const dataLists = {
-      key1:'ab',
-      key2:'bc',
-      key3:'cd',
-      key4:'de',
-    }
+// import componentA from './components/componentA.vue';
+import { ref } from 'vue'
+const test = ref("test");
+const dataList = [
+  'a', 'b', 'c', 'd', '123', 'abcd'
+]
+const dataLists = {
+  key1: 'ab',
+  key2: 'bc',
+  key3: 'cd',
+  key4: 'de',
+}
+
+setTimeout(()=>{
+  test.value = 'apple!'
+},2000)
 </script>
 
 
 <style scoped>
+#app {
+  font-family: Arial, Helvetica, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.test-cls {
+  font-size: 40px;
+  animation: openIn 1s ease-in-out;
+}
+@keyframes openIn{
+  0%{
+    opacity: 0;
+    transform: translatey(20px);
+  }
+  100%{
+    opacity: 1;
+    transform: translatey(0);
+  }
+}
 </style>
