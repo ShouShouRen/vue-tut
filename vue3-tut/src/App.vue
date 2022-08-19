9d8c2c55a3d4a556f40b5a0c3aab791e1999b9db
Peter Chen <peterchen2017125@gmail.com>
2022-08-20 00:02:47 +0800

    20220820

--- vue3-tut/src/App.vue
+++ vue3-tut/src/App.vue
@@ -4,14 +4,24 @@
   <p>{{dataList[1]}}</p>
   <p>{{dataList[2]}}</p>
   <p>{{dataList[3]}}</p> -->
-  <p v-for="text in dataList">{{text}}</p>
+  <!-- <p v-for="text in dataList">{{text}}</p> -->
+  <p v-for="(value,key,index) in dataList">
+    <span v-show="key !== 'key3'">{{index}}{{key}}:{{value}}</span>
+  </p>
+  <!-- <p v-for="text in dataLists">{{text}}</p> -->
 </template>
 
 <script setup>
     // import componentA from './components/componentA.vue';
     const dataList = [
-      'a','b','c','d'
+      'a','b','c','d','123','abcd'
     ]
+    const dataLists = {
+      key1:'ab',
+      key2:'bc',
+      key3:'cd',
+      key4:'de',
+    }
 </script>
 
 
