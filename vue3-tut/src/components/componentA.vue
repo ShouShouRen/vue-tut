7ce3d8a9da01489ee898f39d3f8fed6eb0ba0ca9
Peter <peterchen2017125@gmail.com>
2022-08-19 14:55:42 +0800

    20220819

--- vue3-tut/src/components/componentA.vue
+++ vue3-tut/src/components/componentA.vue
@@ -1,10 +1,22 @@
 <template>
     <p>Hello World</p>
+
+    <p>資料的單向綁定</p>
+    <!-- 資料的單向綁定 -->
     {{ data }}
+
+    <!-- 資料的雙向綁定 -->
+    <p>資料的雙向綁定</p>
+    <input type="text" v-model="text">
+    <p>{{text}}</p>
 </template>
 
 <script setup>
+import { ref } from "@vue/reactivity";
+
 const data = "Hello Vue";
+// ref可使資料同步
+const text = ref("123");
 </script>
 
 
